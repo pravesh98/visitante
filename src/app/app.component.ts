8f84ec48468541d45f9c758cfd4b984beda98ab9
chachlanianjali <anjali-b.chachlani@atos.net>
2018-12-13 13:49:00 +0530

    VTT-1:Added navbar and login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'visitante';
+
+  canLogin: boolean = false;
+
+callingComponent(callingCpt){
+  this.canLogin = callingCpt;
+}
 }
