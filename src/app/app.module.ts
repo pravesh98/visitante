d13676648e4ebdf10b928490b636bc1118425c32
a674543 <pravesh-kumar.shukla@atos.net>
2019-01-03 12:15:14 +0530

    sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
-import {  ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent, DialogOverviewExampleDialog } from './login/login.component';
+import {  ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginComponent,
+    DialogOverviewExampleDialog
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule
   ],
-  providers: [],
+  exports:[
+    MatDialogModule,
+    MatFormFieldModule
+  ],
+  providers: [
+  ],
+  entryComponents:[DialogOverviewExampleDialog],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,46 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject } from '@angular/core';
 import {  FormGroup, FormControl } from '@angular/forms';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
 //import { userInfo } from 'os';
+
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+
+  animal: string;
+  name: string;
 
   loggedIn : boolean = false;
 
-  myform: FormGroup;
-  user: FormControl;
-  pass: FormControl;
+  loginForm=new FormGroup({
+    userName:new FormControl(''),
+    userPassword:new FormControl('')
+  });
 
+  registerForm=new FormGroup({
+    userName:new FormControl(''),
+    userPassword:new FormControl(''),
+    confirmUserPassword:new FormControl('')
+  });
 
-  constructor() { }
+  signInForm:boolean=true;
+  signUpForm:boolean=false;
 
-  ngOnInit() {
+  constructor(public dialog:MatDialog) { }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
   }
 
-  // submit(name, password){
-  //   console.log("tested")
-  //   console.log(name);
-  //   console.log(password);
-  //   this.loggedIn=!this.loggedIn;
-  // }
-
-  // onKeydown(event) {
-  //   console.log(event.value);
-  // } 
-
-  // onSubmit(user) {
-  //   console.log(user);
-
-  //   // var body = "&password=" + user.password + "&name=" + user.name;
-  //   // this.http.post("https://localhost:4200", body).subscribe((data) => {});
-
-  // }
   onSubmitLoginForm(){
     console.log("im in loging component")
-    this.user= name;
+    console.log(this.loginForm.value.userName);
   }
+
+  onSubmitSignUpForm(){
+    console.log("im in loging component")
+    console.log(this.registerForm.value.userName);
+  }
+
+  displayForm(num){
+    if(num==0){
+      this.signInForm=true;
+      this.signUpForm=false;
+    }
+    if(num==1)
+    {      
+      this.signInForm=false;
+      this.signUpForm=true;
+    }
+  }
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
